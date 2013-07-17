0105d3076ec2f938938b3272fe0d71d091659511
smsc <smsc@smsc-desktop.(none)>
2013-07-17 12:04:05 +0530

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '2a1477a4fad80ba31432cbb2603a5d39e97328fdbd32d61964de7be3cc921115f61faabb515c03def6ba0ef32996fd27e0c8bcce24bff4e73484b7d077bbf434'
